3435bb8398a6e59492b98fd9d9349269f65502df
starwispsora <142126413+starwispsora@users.noreply.github.com>
2024-08-21 10:56:44 +0900

    Delete project_14/build directory

--- project_14/build/CMakeFiles/line_follower.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for line_follower.
-# This may be replaced when dependencies are built.
--- project_14/build/CMakeFiles/project_14_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/project_14_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/project_14_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
